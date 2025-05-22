a98f4602da75eea586dc50f71ed6c4f522ee69e5
Vadim0036 <vadim003600@gmail.com>
2025-05-22 02:15:46 -0600

    defined Queue structure / implemented queue init and queue destroy functions

---
+++ src/queue.c
@@ -0,0 +1,49 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "../include/ds_queue.h"
+
+typedef struct Queue
+{
+    void    *queue_arr;     // Dynamically allocated circular array 
+    int     front;          // Index pointing to the front of the array 
+    int     rear;           // Index pointing to the rear of the array 
+    size_t  data_size;      // Size of the data type in bytes 
+    size_t  capacity;       // Current maximum size of the queue 
+    size_t  num_elements;   // Current number of elements in the queue 
+} Queue;
+
+
+Queue *q_init(size_t init_size, size_t data_size)
+{
+    // Validate input 
+    if(init_size == 0 || data_size == 0)
+        return NULL;
+    
+    Queue *new_queue = malloc(sizeof(Queue));
+    if(!new_queue)
+        return NULL;
+    
+    new_queue->queue_arr = calloc(init_size, data_size);
+    if(!new_queue->queue_arr)
+    {
+        free(new_queue);
+        return NULL;
+    }
+
+    new_queue->capacity = init_size;
+    new_queue->front = -1; 
+    new_queue->rear = -1; 
+    new_queue->data_size = data_size;
+    new_queue->num_elements = 0;
+
+    return new_queue;
+}
+
+void q_destroy(Queue *q_ptr)
+{
+    free(q_ptr->queue_arr);
+    free(q_ptr);
+}
