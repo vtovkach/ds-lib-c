54be150f5c7c7e949aa3a3edf357f130c950b8d7
Vadim0036 <vadim003600@gmail.com>
2025-05-23 02:03:23 -0600

    implemented: enqueue (still need to fix bug with resizing), dequeue, peek, get size, get capacity, is empty, is full, clear queue

--- src/queue.c
+++ src/queue.c
@@ -5,6 +5,8 @@
 
 #include "../include/ds_queue.h"
 
+// Fix resizing error in circular queue 
+
 typedef struct Queue
 {
     void    *queue_arr;     // Dynamically allocated circular array 
@@ -49,21 +51,110 @@ void q_destroy(Queue *q_ptr)
 }
 
 
-int q_enqueue(Queue *q_ptr, void *data)
+// Finish double quueu condition 
+
+int q__enqueue(Queue *q_ptr, void *data)
 {
+    // Validate Input 
     if(!q_ptr || !data)
         return -1;
     
+
+    /// FIX ERROR HERE 
     if(q_ptr->num_elements == q_ptr->capacity)
     {
         // double the size 
+        size_t new_size = q_ptr->capacity * q_ptr->data_size * 2;
+
+        void *new_arr = realloc(q_ptr->queue_arr, new_size);
+        if(!new_arr)
+            return 2; // Indicate that queue is full and can't be resized 
+        
+        q_ptr->queue_arr = new_arr;
+        q_ptr->capacity *= 2; 
     }
 
-    
-    int rear = (q_ptr->rear + 1) % q_ptr->capacity;
-    
+    if(q_ptr->num_elements == 0)
+    {
+        q_ptr->front = 0;
+        q_ptr->rear = 0;
+    }
+    else
+    {
+        q_ptr->rear = (q_ptr->rear + 1) % q_ptr->capacity;
+    }
 
-    memcpy();
+    memcpy((uint8_t *)q_ptr->queue_arr + q_ptr->rear * q_ptr->data_size, data, q_ptr->data_size);
+
+    q_ptr->num_elements++;
 
     return 0; 
+}
+
+int q_dequeue(Queue *q_ptr, void *dest)
+{
+    // Validate input 
+    if(!q_ptr || !dest)
+        return -1; 
+
+    if(q_ptr->num_elements == 0)
+        return 1; // indicate that queue is empty 
+    
+    memcpy(dest, (uint8_t *)q_ptr->queue_arr + q_ptr->front * q_ptr->data_size, q_ptr->data_size);
+
+    q_ptr->front = (q_ptr->front + 1) % q_ptr->capacity;
+    q_ptr->num_elements--;
+
+    return 0;
+}
+
+int q_peek(Queue *q_ptr, void *dest)
+{
+    if(!q_ptr || !dest)
+        return -1;
+
+    if(q_ptr->num_elements == 0)
+        return 1; // Indicate empty queue 
+
+    memcpy(dest, (uint8_t *)q_ptr->queue_arr + q_ptr->front * q_ptr->data_size, q_ptr->data_size);
+
+    return 0;
+}
+
+size_t q_size(Queue *q_ptr)
+{
+    return q_ptr ? q_ptr->num_elements : 0;
+}
+
+size_t q_capacity(Queue *q_ptr)
+{
+    return q_ptr ? q_ptr->capacity : 0;
+}
+
+int q_empty(Queue *q_ptr)
+{
+    if(!q_ptr || q_ptr->num_elements == 0)
+        return 1;
+    
+    return 0; 
+}
+
+int q_full(Queue *q_ptr)
+{
+    if(!q_ptr || q_ptr->num_elements != q_ptr->capacity)
+        return 0; 
+
+    return 1; 
+}
+
+int q_clear(Queue *q_ptr)
+{
+    if(!q_ptr)
+        return -1; 
+    
+    q_ptr->front = -1; 
+    q_ptr->rear = -1;
+    q_ptr->num_elements = 0;
+
+    return 0;
 }
