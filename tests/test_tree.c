9a864b24bd46f56e0d1ff141de847b88314d683d
Vadim0036 <vadim003600@gmail.com>
2025-05-24 16:44:26 -0600

    removed extra code

--- tests/test_tree.c
+++ tests/test_tree.c
@@ -2,47 +2,6 @@
 #include <time.h>
 #include "../include/ds_tree.h"
 
-/*
-int compare(void *left_ptr, void *right_ptr)
-{
-    int left = *(int *)left_ptr;
-    int right = *(int *)right_ptr;
-
-    if(left == right)
-        return 0;
-    else if(left > right)
-        return 1; 
-    else 
-        return -1;
-}
-
-void print_data(void *data)
-{
-    if(!data)
-    {
-        return;
-    }
-    else
-    {
-        printf("%d ", *(int *)data);
-    }
-}
-
-
-int main()
-{
-    AVL_Tree *tree = avl_create(sizeof(int), 1, compare);
-
-    tree_insert(tree, 5);
-    tree_insert(tree, 10);
-
-    traverse_tree(tree, print_data, TRAVERSE_INORDER);
-
-    avl_destroy(tree);
-}
-*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
