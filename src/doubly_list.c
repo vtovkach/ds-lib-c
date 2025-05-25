b504df1f45ee141937d5d5632c6802a948d7072f
Vadym <vadim@Vadyms-MacBook-Air.local>
2025-05-25 15:17:57 -0500

    implemented init, destroy, push_front, push_back, pop_front for doubly linked list

---
+++ src/doubly_list.c
@@ -0,0 +1,179 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/ds_doubly_list.h"
+
+typedef struct Node
+{
+    void *data; 
+    struct Node *nextptr; 
+    struct Node *prevptr;     
+} Node;
+
+typedef struct D_List
+{
+    Node *front; 
+    Node *rear; 
+    size_t num_elements; 
+    size_t data_size; 
+} D_List;
+
+
+D_List *d_list_init(size_t data_size)
+{
+    if(data_size == 0)
+        return NULL; 
+    
+    D_List *new_list = malloc(sizeof(D_List));
+    if(!new_list)
+        return NULL; 
+    
+    new_list->front = NULL;
+    new_list->rear = NULL;
+    new_list->num_elements = 0;
+    new_list->data_size = data_size; 
+
+    return new_list; 
+}
+
+void d_list_destroy(D_List *l_ptr)
+{
+    if(!l_ptr)
+        return;
+
+    // Free all nodes 
+    Node *cur_node = l_ptr->front;
+    Node *next = NULL;
+    while(cur_node != NULL)
+    {
+        next = cur_node->nextptr; 
+        
+        free(cur_node->data);
+        free(cur_node);
+
+        cur_node = next; 
+    }
+
+    // Free list itself 
+    free(l_ptr);
+}
+
+int d_l__push_front(D_List *l_ptr, void *data)
+{
+    if(!l_ptr || !data)
+        return -1; 
+    
+    Node *new_node = malloc(sizeof(Node));
+    if(!new_node)
+        return -1; 
+
+    new_node->data = malloc(l_ptr->data_size);
+    if(!new_node->data)
+    {
+        free(new_node);
+        return -1;
+    }
+    
+    memcpy(new_node->data, data, l_ptr->data_size);
+
+    new_node->prevptr = NULL;
+    new_node->nextptr = l_ptr->front;
+    
+    if(l_ptr->num_elements == 0)
+    {
+        l_ptr->front = new_node;
+        l_ptr->rear = new_node;
+        l_ptr->num_elements++;
+        return 0; 
+    }    
+    
+    l_ptr->front->prevptr = new_node;
+    l_ptr->front = new_node;
+
+    l_ptr->num_elements++;
+
+    return 0;
+}
+
+int d_l__push_back(D_List *l_ptr, void *data)
+{
+    if(!l_ptr || !data)
+        return -1; 
+
+    if(l_ptr->num_elements == 0)
+        return d_l__push_front(l_ptr, data);
+
+    Node *new_node = malloc(sizeof(Node));
+    if(!new_node)
+        return -1;  
+
+    new_node->data = malloc(l_ptr->data_size);
+    if(!new_node->data)
+    {
+        free(new_node); 
+        return -1;
+    }
+    
+    memcpy(new_node->data, data, l_ptr->data_size);
+    
+    new_node->nextptr = NULL;
+    new_node->prevptr = l_ptr->rear;
+    
+    l_ptr->rear->nextptr = new_node;
+    l_ptr->rear = new_node; 
+    
+    l_ptr->num_elements++;
+
+    return 0;
+}
+
+int d_l__push_at(D_List *l_ptr, void *data, size_t index)
+{
+    // 
+    
+}
+
+int d_l_pop_front(D_List *l_ptr, void *dest)
+{
+    if(!l_ptr || !dest)
+        return -1; 
+
+    if(l_ptr->num_elements == 0)
+        return 1; // List is empty
+
+    memcpy(dest, l_ptr->front->data, l_ptr->data_size);
+
+    if(l_ptr->num_elements == 1)
+    {
+        free(l_ptr->front->data);
+        free(l_ptr->front);
+
+        l_ptr->front = NULL;
+        l_ptr->rear = NULL;
+        l_ptr->num_elements = 0;
+
+        return 0;
+    }   
+
+    Node *temp = l_ptr->front;
+
+    l_ptr->front = temp->nextptr;
+    l_ptr->front->prevptr = NULL;
+    
+    free(temp->data);
+    free(temp);
+
+    l_ptr->num_elements--; 
+
+    return 0;
+}
+
+int d_l_pop_back(D_List *l_ptr, void *dest)
+{
+
+}
+
+int d_l_pop_at(D_List *l_ptr, void *data, size_t index)
+{
+
+}
