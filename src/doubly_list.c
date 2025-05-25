6964647b2869d9a0ef0a21842b057fca2e5c78c8
Vadym <vadim@Vadyms-MacBook-Air.local>
2025-05-25 17:00:25 -0500

    implemented push_at and pop_at functions for doubly linked list

--- src/doubly_list.c
+++ src/doubly_list.c
@@ -129,8 +129,46 @@ int d_l__push_back(D_List *l_ptr, void *data)
 
 int d_l__push_at(D_List *l_ptr, void *data, size_t index)
 {
-    // 
+    if(!l_ptr || !data)
+        return -1; 
     
+    if(index >= l_ptr->num_elements)
+        return -2; // Index is out of bounds 
+
+    if(l_ptr->num_elements == 0)
+        return 1; // List is empty 
+
+    if(index == 0)
+        return d_l__push_front(l_ptr, data); 
+
+    if(index == l_ptr->num_elements - 1)
+        return d_l__push_back(l_ptr, data);
+
+    Node *new_node = malloc(sizeof(Node));
+    if(!new_node)
+        return -1; 
+
+    // Prepare new node 
+    new_node->data = malloc(l_ptr->data_size);
+    memcpy(new_node->data, data, l_ptr->data_size);
+    new_node->nextptr = NULL;
+    new_node->prevptr = NULL;
+
+    Node *cur_node = l_ptr->front;
+    size_t count = 0;
+    while(count++ < index)
+    {
+        cur_node = cur_node->nextptr;
+    }
+
+    new_node->nextptr = cur_node;
+    new_node->prevptr = cur_node->prevptr;
+
+    cur_node->prevptr = new_node;
+    
+    l_ptr->num_elements++;
+
+    return 0; 
 }
 
 int d_l_pop_front(D_List *l_ptr, void *dest)
@@ -170,10 +208,63 @@ int d_l_pop_front(D_List *l_ptr, void *dest)
 
 int d_l_pop_back(D_List *l_ptr, void *dest)
 {
+    if(!l_ptr)
+        return -1; 
 
+    if(l_ptr->num_elements == 0)
+        return 1; 
+
+    if(l_ptr->num_elements == 1)
+        return d_l_pop_front(l_ptr, dest);
+
+    memcpy(dest, l_ptr->rear->data, l_ptr->data_size);
+    
+    Node *temp = l_ptr->rear;
+
+    l_ptr->rear = l_ptr->rear->prevptr;
+    l_ptr->rear->nextptr = NULL;
+
+    free(temp->data);
+    free(temp);
+
+    l_ptr->num_elements--;
+
+    return 0;
 }
 
-int d_l_pop_at(D_List *l_ptr, void *data, size_t index)
+int d_l_pop_at(D_List *l_ptr, void *dest, size_t index)
 {
+    if(!l_ptr || !dest)
+        return -1; 
 
+    if(l_ptr->num_elements == 0)
+        return 1; // List is empty 
+
+    if(index >= l_ptr->num_elements)
+        return -2; // Index is out of bounds 
+
+    if(index == 0)
+        return d_l_pop_front(l_ptr, dest);
+    
+    if(index == l_ptr->num_elements - 1)
+        return d_l_pop_back(l_ptr, dest);
+
+    Node *cur_node = l_ptr->front;
+    size_t count = 0;
+    while(count++ < index)
+    {
+        cur_node = cur_node->nextptr;
+    }
+
+    memcpy(dest, cur_node->data, l_ptr->data_size);
+
+    cur_node->nextptr->prevptr = cur_node->prevptr;
+    cur_node->prevptr->nextptr = cur_node->nextptr;
+
+    free(cur_node->data);
+    free(cur_node);
+
+    l_ptr->num_elements--;
+
+    return 0;
 }
