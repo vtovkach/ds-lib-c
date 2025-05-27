1941460178d956bef947546581f77f27a4ce0c94
Vadim0036 <vadim003600@gmail.com>
2025-05-26 22:25:33 -0600

    renamed a few functions to avoid naming conflicts

--- include/ds_doubly_list.h
+++ include/ds_doubly_list.h
@@ -22,19 +22,19 @@ int d_l_pop_back(D_List *l_ptr, void *dest);
 
 int d_l_pop_at(D_List *l_ptr, void *dest, size_t index);
 
-int l_peek_front(D_List *l_ptr, void *dest);
+int d_l_peek_front(D_List *l_ptr, void *dest);
 
-int l_peek_back(D_List *l_ptr, void *dest);
+int d_l_peek_back(D_List *l_ptr, void *dest);
 
-int l_peek_at(D_List *l_ptr, void *dest, size_t index);
+int d_l_peek_at(D_List *l_ptr, void *dest, size_t index);
 
-int l_empty(D_List *l_ptr);
+int d_l_empty(D_List *l_ptr);
 
-int l_clear(D_List *l_ptr);
+int d_l_clear(D_List *l_ptr);
 
-int l_reverse(D_List *l_ptr);
+int d_l_reverse(D_List *l_ptr);
 
-size_t l_size(D_List *l_ptr);
+size_t d_l_size(D_List *l_ptr);
 
 
 #endif 
