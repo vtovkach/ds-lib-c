c40146e0a050d72be1554c464a0475714f10d4fb
Vadim0036 <vadim003600@gmail.com>
2025-05-29 23:39:01 -0600

    created Vector structure, implemented vector_create, vector_destroy

--- src/vector.c
+++ src/vector.c
@@ -1,2 +1,42 @@
+#include <string.h>
+#include <stdlib.h>
 
 #include "../include/ds_vector.h"
+
+typedef struct Vector 
+{
+    void    *vec_array;     // dynamic array 
+    size_t  data_size;      // data size in bytes 
+    size_t  num_elements;   // current number of elements 
+    size_t  capacity;       // current capacity of the list 
+
+} Vector;
+
+Vector *vec_create(size_t capacity, size_t data_size)
+{
+    if(capacity == 0 || data_size == 0)
+        return NULL;
+
+    Vector *new_vec = malloc(sizeof(Vector));
+    if(!new_vec)
+        return NULL; 
+
+    new_vec->vec_array = malloc(capacity * data_size);
+    if(!new_vec->vec_array)
+    {
+        free(new_vec);
+        return NULL;
+    }
+
+    new_vec->data_size = data_size;
+    new_vec->capacity = capacity;
+    new_vec->num_elements = 0;
+
+    return new_vec;
+}
+
+void vec_destroy(Vector *vec)
+{
+    free(vec->vec_array);
+    free(vec);
+}
