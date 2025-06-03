c6803a8f445c7803b0f06dace1060783e31db16d
Baristan Berotti <vadim003600@gmail.com>
2025-06-03 18:12:43 -0500

    added master header file

---
+++ include/ds_lib.h
@@ -0,0 +1,13 @@
+#ifndef DS_LIB_H
+#define DS_LIB_H
+
+#include "ds_vector.h"
+#include "ds_singly_list.h"
+#include "ds_doubly_list.h"
+#include "ds_stack.h"
+#include "ds_queue.h"
+#include "ds_tree.h"
+#include "ds_hashtable.h"
+#include "ds_heap.h"
+
+#endif 
